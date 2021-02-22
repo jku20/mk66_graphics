1dd9c0c88b796aa4171dc257f87a61a97bf0b3ec
jku20 <jku-ben20@stuy.edu>
2021-02-21 22:07:40 -0500

    made a cool image apear

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include "drawer.h"
 
 #include <time.h>
+#include <stdlib.h>
 
 #include <stdio.h>
 #include <string.h>
@@ -68,14 +69,77 @@ void line_test() {
     write_PPM((unsigned char *) img, W, H, "test.ppm");
 }
 
+const int rand1[7] = {-1,-1,-1,-1,1,1,0};
+const int rand2[7] = {1,1,1,-1,-1,-1,0};
+const int color_rand[6] = {-1,-1,-1,1,1,1};
+const int rounds = 13;
+const int step = 2;
+
+void squiggle(int sx, int sy, unsigned char c[3]) {
+    while(sx < W && sy < H && sx >= 0 && sy >= 0) {
+        //printf("sx: %d, sy: %d\n", sx, sy);
+        int i;
+        for(i = 0; i < rounds/2 && sx < W && sy < H && sx >= 0 && sy >= 0; ++i) {
+            int nx = sx, ny = sy;
+            nx += (2 + rand1[rand() % 7]) * step;
+            ny += rand1[rand() % 7] * step;
+
+            nx = (nx >= W) ? W-1 : nx;
+            ny = (ny >= H) ? H-1 : ny;
+            nx = (nx < 0) ? 0 : nx;
+            ny = (ny < 0) ? 0 : ny;
+
+            draw_line(sx, sy, nx, ny, W, (unsigned char *)img, c);
+            sx = nx, sy = ny;
+
+            int j;
+            for(j = 0; j < RGB_NUM; j++) {
+                c[j] += color_rand[rand() % 3];
+            }
+        }
+        for(i = 0; i < rounds/2 && sx < W && sy < H && sx >= 0 && sy >= 0; ++i) {
+            int nx = sx, ny = sy;
+            nx += (2 + rand2[rand() % 7]) * step;
+            ny += rand2[rand() % 7] * step;
+
+            nx = (nx >= W) ? W-1 : nx;
+            ny = (ny >= H) ? H-1 : ny;
+            nx = (nx < 0) ? 0 : nx;
+            ny = (ny < 0) ? 0 : ny;
+
+
+            draw_line(sx, sy, nx, ny, W, (unsigned char *)img, c);
+            sx = nx, sy = ny;
+
+            int j;
+            for(j = 0; j < RGB_NUM; j++) {
+                c[j] += color_rand[rand() % 3];
+            }
+
+        }
+
+
+        if(sx == W - 1) return;
+        squiggle(sx, sy, c);
+    }
+}
+
 void cool_image() {
     memset(img, 0, sizeof(img));
+
+    char col[3] = {0,0,0};
+    squiggle(0, H/2, col);
+
+    write_PPM((unsigned char *) img, W, H, "test.ppm");
 }
 
 int main() {
     const clock_t start = clock();
 
+    srand(time(NULL));
+
     line_test();
+    //cool_image();
 
     const clock_t end = clock();
     printf("time: %lfsec\n", (double)(end - start)/CLOCKS_PER_SEC);
